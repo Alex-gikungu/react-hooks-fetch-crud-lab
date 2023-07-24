3684b1f310a5ed1f516f1ac6a55675c58ec7f207
Alex-gikungu <alexigikungu.012@gmail.com>
2023-07-24 23:26:33 +0300

    completed task

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,64 @@
-import React from "react";
+// import React{useState,} from "react";
+
+// function QuestionList() {
+//   return (
+//     <section>
+//       <h1>Quiz Questions</h1>
+//       <ul>{/* display QuestionItem components here after fetching */}</ul>
+//     </section>
+//   );
+// }
+
+// export default QuestionList;
+
+
+
+
+
+
+
+import React, { useState, useEffect } from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = () => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  };
+
+  const handleDeleteClick = (id) => {
+    deleteQuestion(id);
+  };
+
+  const deleteQuestion = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setQuestions(questions.filter((question) => question.id !== id));
+      })
+      .catch((error) => console.error("Error deleting question:", error));
+  };
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <div>
+      <h1>Questions</h1>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>
+            {question.prompt}
+            <button onClick={() => handleDeleteClick(question.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
